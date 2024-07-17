b8ef55110dfc2360004e75d06660d7d3f45248d4
anthonyesper <51342402+anthonyesper@users.noreply.github.com>
2024-07-17 10:19:30 -0400

    Update route_53_resolver.tf

--- route_53_resolver.tf
+++ route_53_resolver.tf
@@ -5,7 +5,8 @@
 data "aws_route53_resolver_rules" "shared_rules" {
   share_status = "SHARED_WITH_ME"
 }
-data "aws_vpcs" "all_vpcs"{}
+
+data "aws_vpcs" "all_vpcs" {}
 
 locals {
   resolver_rule_ids = data.aws_route53_resolver_rules.shared_rules.resolver_rule_ids
@@ -13,14 +14,28 @@ locals {
   rule_vpc_pairs    = toset(setproduct(local.resolver_rule_ids, local.vpc_ids))
 }
 
+// Fetch existing rule associations
+data "aws_route53_resolver_rule_associations" "existing_associations" {}
+
+// Create a set of existing rule-vpc associations
+locals {
+  existing_associations = toset([
+    for assoc in data.aws_route53_resolver_rule_associations.existing_associations.associations :
+    "${assoc.resolver_rule_id}-${assoc.vpc_id}"
+  ])
+}
+
+// Filter pairs to create only those associations that do not already exist
+locals {
+  new_rule_vpc_pairs = [for pair in local.rule_vpc_pairs : pair if !contains(local.existing_associations, "${pair[0]}-${pair[1]}")]
+}
+
 resource "aws_route53_resolver_rule_association" "vpc_associations" {
-  for_each = { for pair in local.rule_vpc_pairs : "${pair[0]}-${pair[1]}" => pair }
+  for_each = { for pair in local.new_rule_vpc_pairs : "${pair[0]}-${pair[1]}" => pair }
   resolver_rule_id = each.value[0]
   vpc_id           = each.value[1]
 }
 
 output "generated_combinations" {
-  value = local.rule_vpc_pairs
+  value = local.new_rule_vpc_pairs
 }
-
-
